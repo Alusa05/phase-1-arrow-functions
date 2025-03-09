6a8156d94954c1cd6e3383a9e7ead0a4bd461c64
Lisa Alusa <sayilisaalusa@gmail.com>
2025-03-09 19:47:13 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,10 @@
+// Function Expression: divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Arrow Function: square
+  const square = x => x * x;
+  
+  // Arrow Function: add
+  const add = (a, b) => a + b;
